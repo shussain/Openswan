624d6b4e08445557b83473c2dcb28a95b931e0e8
Michael Richardson <mcr@xelerance.com>
2006-12-08 15:13:26 -0500

    test case for setting an IPCOMP algorithm in the phase2alg=
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

---
+++ testing/pluto/ipcomp-01/eastinit.sh
@@ -0,0 +1,8 @@
+: ==== start ====
+TESTNAME=ipcomp-01
+source /testing/pluto/bin/eastlocal.sh
+
+ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add westnet-eastnet-comp1
+
