bb009c64e8137df8a1efc912c901114155cf70ef
Michael Richardson <mcr@credil.org>
2018-02-18 20:56:21 -0500

    wo#6839 . first compilable lp74

--- tests/unit/libpluto/lp74-parentM2/parentM2.c
+++ tests/unit/libpluto/lp74-parentM2/parentM2.c
@@ -1,12 +1,22 @@
+#define INCLUDE_IKEV1_PROCESSING
+#define OMIT_MAIN_MODE
 #define NAPT_ENABLED 1
 #define SEAM_CRYPTO
 #include "../lp10-parentI2/parentI2_head.c"
 #include "seam_rsasig.c"
 #include "seam_keys.c"
 #include "seam_x509.c"
+#include "seam_dpd.c"
+#include "seam_ikev1_aggr.c"
+#include "seam_ikev1_phase2.c"
+#include "seam_unpend.c"
+#include "seam_command.c"
+#include "seam_rsa_check.c"
 #include "seam_host_parker.c"
 
-#define TESTNAME "h2hI2"
+#define TESTNAME "parentM2"
+
+bool no_cr_send = 0;
 
 static void init_local_interface(void)
 {
