8eba84ef473d67961b74123edbd85431c405abb8
Michael Richardson <mcr@credil.org>
2018-03-04 18:59:57 -0500

    wo#6839 . added kernel interface output with correct number of bits of phase 2 key

--- tests/unit/xfrmmock/xf79-childQR1/xf79-childQR1.c
+++ tests/unit/xfrmmock/xf79-childQR1/xf79-childQR1.c
@@ -42,7 +42,9 @@ static void init_fake_secrets(void)
 }
 
 static void init_loaded(void)
-{   /* nothing */ }
+{
+    xfrm_init_base_algorithms();
+}
 
 
 #define PCAP_INPUT_COUNT 4
@@ -55,6 +57,27 @@ recv_pcap recv_inputs[PCAP_INPUT_COUNT]={
     recv_pcap_packet2ikev1,
     recv_pcap_packet3ikev1,
 };
+
+#define FINISH_NEGOTIATION
+static void finish_negotiation(void)
+{
+    volatile struct state *st;
+    st = state_with_serialno(1);
+    passert(st != NULL);
+
+    passert(st->st_oakley.integ_hash == IKEv2_AUTH_HMAC_SHA1_96);
+    passert(st->st_oakley.prf_hash   == IKEv2_AUTH_HMAC_SHA1_96);
+    passert(st->st_oakley.encrypt    == IKEv2_ENCR_AES_CBC);
+    passert(st->st_oakley.enckeylen  == 128);
+
+    st = state_with_serialno(2);
+    passert(st != NULL);
+
+    passert(st->st_esp.present);
+    passert(st->st_esp.attrs.transattrs.integ_hash == IKEv2_AUTH_HMAC_SHA1_96);
+    passert(st->st_esp.attrs.transattrs.encrypt    == IKEv2_ENCR_AES_CBC);
+    passert(st->st_esp.attrs.transattrs.enckeylen  == 128);
+}
 
 #include "../lp12-parentR2/parentR2_main.c"
 
