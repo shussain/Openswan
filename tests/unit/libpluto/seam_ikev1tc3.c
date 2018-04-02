001f6fe1ba0f6319d3bcc21b1c41a6baec1b1cdb
Michael Richardson <mcr@credil.org>
2018-04-02 19:53:50 -0400

    wo#6839 . remove unused recv_pcap_packet1ikev1

--- tests/unit/libpluto/seam_ikev1tc3.c
+++ tests/unit/libpluto/seam_ikev1tc3.c
@@ -22,3 +22,42 @@ void recv_pcap_packet(u_char *user
 
     run_continuation(crypto_req);
 }
+
+void recv_pcap_packet2ikev1(u_char *user
+                      , const struct pcap_pkthdr *h
+                      , const u_char *bytes)
+{
+    struct state *st;
+    struct pcr_kenonce *kn = &crypto_req->pcr_d.kn;
+
+    recv_pcap_packet_gen(user, h, bytes);
+
+    /* find st involved */
+    st = state_with_serialno(1);
+    if(st) {
+      st->st_connection->extra_debugging = DBG_PRIVATE|DBG_CRYPT|DBG_PARSING|DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE;
+      update_ngi(kn);
+      run_continuation(crypto_req);
+    }
+}
+
+void recv_pcap_packet3ikev1(u_char *user
+                      , const struct pcap_pkthdr *h
+                      , const u_char *bytes)
+{
+    struct state *st;
+    struct pcr_kenonce *kn = &crypto_req->pcr_d.kn;
+
+    /* before receiving the packet, need to complete the async calculation of the g^xy */
+
+
+    cur_debugging |= DBG_PRIVATE|DBG_CRYPT|DBG_PARSING|DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE;
+    recv_pcap_packet_gen(user, h, bytes);
+
+    /* find st involved */
+    st = state_with_serialno(1);
+    if(st) {
+      run_continuation(crypto_req);
+    }
+}
+
